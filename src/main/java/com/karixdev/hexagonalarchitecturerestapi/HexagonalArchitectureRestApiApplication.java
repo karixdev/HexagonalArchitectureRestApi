f98cc683eb85e1ad549cf508998e044e02ec5ac6
karixdev <kari.klucz@gmail.com>
2023-07-14 23:38:19 +0200

    init

---
+++ src/main/java/com/karixdev/hexagonalarchitecturerestapi/HexagonalArchitectureRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.karixdev.hexagonalarchitecturerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitectureRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karixdev/hexagonalarchitecturerestapi/HexagonalArchitectureRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karixdev.hexagonalarchitecturerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
